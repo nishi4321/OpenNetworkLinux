58378ca58747690fc95992ab22cb01f48be552db
nishi4321 <n.nishikita2@gmail.com>
2021-05-31 01:24:07 +0900

    Fix sprintf error.

--- packages/platforms/delta/x86-64/agc032a/onlp/builds/x86_64_delta_agc032a/module/src/sysi.c
+++ packages/platforms/delta/x86-64/agc032a/onlp/builds/x86_64_delta_agc032a/module/src/sysi.c
@@ -484,7 +484,7 @@ int onlp_sysi_platform_info_get(onlp_platform_info_t *pi)
 {
     int rv = 0, cpld_idx = 0;
     uint8_t cpld_version[cpld_list_size];
-    char buffer[256] = "";
+    char buffer[65536] = "";
 
     void *busDrv = NULL;
     cpld_dev_driver_t *cpld = (cpld_dev_driver_t *)vendor_find_driver_by_name("CPLD");
@@ -509,7 +509,7 @@ int onlp_sysi_platform_info_get(onlp_platform_info_t *pi)
             return ONLP_STATUS_E_INTERNAL;
         }
 
-        char tmpBuffer[256] = "";
+        char tmpBuffer[65536] = "";
         strcpy(tmpBuffer, buffer);
         sprintf(buffer, "%s \n\t\t%s: 0x%02x ", tmpBuffer, cpld_version_list[cpld_idx].name, cpld_version[cpld_idx]);
         // sprintf(buffer, "%s \n\t\t%s: 0x%02x ", buffer, cpld_version_list[cpld_idx].name, cpld_version[cpld_idx]);
