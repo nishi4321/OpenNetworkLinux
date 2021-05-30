f11d70f26cbbfa9b8782f58565fd43921747990a
nishi4321 <n.nishikita2@gmail.com>
2021-05-31 05:07:34 +0900

    Add -Wno-restrict

--- packages/base/any/onlp/src/onlplib/module/make.mk
+++ packages/base/any/onlp/src/onlplib/module/make.mk
@@ -31,5 +31,6 @@ onlplib_DEPENDMODULE_ENTRIES := init:onlplib
 ifeq ($(ONL_DEBIAN_SUITE),buster)
 GLOBAL_CFLAGS += -DONLPLIB_CONFIG_I2C_INCLUDE_SMBUS=1
 GLOBAL_CFLAGS += -Wno-implicit-function-declaration
+GLOBAL_CFLAGS += -Wno-restrict
 GLOBAL_LINK_LIBS += -li2c
 endif
